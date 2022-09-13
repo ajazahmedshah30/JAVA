6a866ceb529320a13bf97372b40c0ac079552e1f
Ajaz Ahmed Shah <95103741+ajazahmedshah30@users.noreply.github.com>
2022-09-13 11:18:11 +0530

    Update and rename README.md to first.java

---
+++ first.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+//WAP to add three numbers in Java
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 4;
+        int b = 17;
+        int c =6;
+        int sum = a + b + c;
+        System.out.println(sum);
+
+
+    }
+}
