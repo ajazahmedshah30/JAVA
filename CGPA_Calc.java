cfc342bea6896786979fac1cbdbc252fb1f8fd56
Ajaz Ahmed Shah <95103741+ajazahmedshah30@users.noreply.github.com>
2022-09-13 11:27:46 +0530

    Create CGPA_Calc.java

---
+++ CGPA_Calc.java
@@ -0,0 +1,15 @@
+//WAP to calculate CGPA using marks of five subjects (Marks out of 100 per Subject)
+
+Class CGPA
+{
+    public static void main(String[] args) 
+    {
+        float subject1 = 45;
+        float subject2 = 95;
+        float subject3 = 77;
+        float subject2 = 85;
+        float subject3 = 48;
+        float cgpa = (subject1 + subject2 + subject3 + subject4 + subject5)/50;
+        System.out.println(cgpa);
+     }
+}
