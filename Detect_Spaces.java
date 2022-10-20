5d92e4e16ed63b7367d21f71be0887bbf7b26a54
Ajaz Ahmed Shah <95103741+ajazahmedshah30@users.noreply.github.com>
2022-10-20 14:23:27 +0530

    Create Detect_Spaces.java

---
+++ Detect_Spaces.java
@@ -0,0 +1,12 @@
+    //WAP to detect double and triple spaces in a string. 
+  
+     public class Detectspaces { 
+         public static void main(String[] args) { 
+             String myString = "This string contains double and  triple   spaces"; 
+             System.out.println(myString.indexOf("  ")); 
+             System.out.println(myString.indexOf("   ")); 
+  
+  
+     } 
+  
+ }
