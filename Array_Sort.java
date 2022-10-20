16df64b2ecb2c45be6ddb977ec7d93ac6da36304
Ajaz Ahmed Shah <95103741+ajazahmedshah30@users.noreply.github.com>
2022-10-20 14:20:32 +0530

    Create Array_Sort.java

---
+++ Array_Sort.java
@@ -0,0 +1,21 @@
+//Write a Java program to find whether an array is sorted or not. 
+ public class Array_Sort{ 
+     public static void main(String[] args) { 
+         boolean isSorted = true; 
+         int [] arr = {1, 12, 3, 4, 5, 34, 67}; 
+         for(int i=0;i<arr.length-1;i++){ 
+             if(arr[i] > arr[i+1]){ 
+                 isSorted = false; 
+                 break; 
+             } 
+         } 
+         if(isSorted){ 
+             System.out.println("The Array is sorted"); 
+         } 
+         else{ 
+             System.out.println("The Array is not sorted"); 
+         } 
+  
+  
+     } 
+     }
