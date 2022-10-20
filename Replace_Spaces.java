a8439f53232167f5aacad8afd9da6650305352df
Ajaz Ahmed Shah <95103741+ajazahmedshah30@users.noreply.github.com>
2022-10-20 14:29:01 +0530

    Create Replace_Spaces.java

---
+++ Replace_Spaces.java
@@ -0,0 +1,9 @@
+//Write a Java program to replace spaces with underscores. 
+  
+ public class Underscores { 
+     public static void main(String[] args) { 
+         String text = " Ishfaq ah mad    gojree roll no 04 "; 
+         text = text.replace(" ", "_"); 
+         System.out.println(text); 
+     } 
+ }
